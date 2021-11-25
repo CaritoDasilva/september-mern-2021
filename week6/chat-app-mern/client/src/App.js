647f9a8dd7108b928345f6d88a072fe35dfe8119
caritodasilva <caritodasilva@gmail.com>
2021-11-25 15:44:44 -0300

    algos array

--- week6/chat-app-mern/client/src/App.js
+++ week6/chat-app-mern/client/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useHistory
 } from "react-router-dom";
 import axios from 'axios';
 import Login from './views/Login';
@@ -13,43 +14,52 @@ import Users from './views/Users';
 import Profile from './views/Profile';
 import CreateProfile from './views/CreateProfile';
 import SocketProvider from './contexts/SocketContext';
+import LogindAndRegister from './views/LoginAndRegister';
+import { AuthContext, AuthProvider } from './contexts/AuthContext';
 
 function App() {
+  const { isLogued, setIsLogued, user, setUser } = useContext(AuthContext);
+  const history = useHistory();
   //Todo:
   //Crear contexto para el usuario
   //Crear login 
   //Agregar funcionalidad para cuando se desloguee se desconecte del socket!!!!
 
-  // const logout = async () => {
-  //   try {
-  //         await axios.put(`http://localhost:8080/api/users/${id}`, { isOnline: false })
+  const logout = async () => {
+    try {
+          await axios.put(`http://localhost:8080/api/users/${user?.id}`, { isOnline: false })
+          setIsLogued(false);
+          setUser();
+          history.push('/login')
 
-  //       } catch(err) {
-  //           return err;
-  //       }
-  // }
+        } catch(err) {
+            return err;
+        }
+  }
   
   return (
     <div className="App">
-      {/* <button onClick={logout}>Logout</button> */}
-      <SocketProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/login">
-              <Login/>
-            </Route>
-            <Route exact path="/users">
-              <Users/>
-            </Route>
-            <Route exact path="/user/:id">
-              <Profile/>
-            </Route>
-            <Route exact path="/create-profile/:id">
-              <CreateProfile/>
-            </Route>
-          </Switch>
-        </Router>
-      </SocketProvider>
+      <AuthProvider>
+        <SocketProvider>
+          <Router>
+            <button onClick={logout}>Logout</button>
+            <Switch>
+              <Route exact path="/login">
+                <LogindAndRegister/>
+              </Route>
+              <Route exact path="/users">
+                <Users/>
+              </Route>
+              <Route exact path="/user/:id">
+                <Profile/>
+              </Route>
+              <Route exact path="/create-profile/:id">
+                <CreateProfile/>
+              </Route>
+            </Switch>
+          </Router>
+        </SocketProvider>
+      </AuthProvider>
       
     </div>
   );
